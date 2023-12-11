6d6233224a1518ad8bff82e81f0f497a5ebd3fa6
GraceSevillano <52088906+GraceSevillano@users.noreply.github.com>
2023-12-11 03:14:20 +0100

    Update Niryo.py

--- turtlebot3_autorace_driving/nodes/Niryo.py
+++ turtlebot3_autorace_driving/nodes/Niryo.py
@@ -12,30 +12,27 @@ class NiryoConnection:
 
         self.sub_niryo = rospy.Subscriber("/niryo_con",UInt8, self.communicate_with_niryo,queue_size=1)
         self.pub_niryo = rospy.Publisher("/niryo_con",UInt8, queue_size=10)
-        self.counter = 0
 
     def communicate_with_niryo(self, msg):
-        self.counter += 1
-        if self.counter <= 1:
-            print(msg.data)
-            if msg.data ==1:
-                robot = NiryoRobot("192.168.0.150")
+        print(msg.data)
+        if msg.data ==1:
+            robot = NiryoRobot("192.168.0.150")
 
-                try:
+            try:
                     #move to an observation position
-                    robot.move_pose(*[0.001,-0.213,0.217,3.1,1.395, 1.559])
+                robot.move_pose(*[0.001,-0.213,0.217,3.1,1.395, 1.559])
 
-                    if robot.vision_pick("default_workspace_turtlebot",0/1000.0,ObjectShape.CIRCLE,ObjectColor.RED)[0]:
+                if robot.vision_pick("default_workspace_turtlebot",0/1000.0,ObjectShape.CIRCLE,ObjectColor.RED)[0]:
                         # If an object has been taken, do:
-                        robot.place_from_pose(*[0.326,-0.015,0.314,-2.232,1.471,-2.234])
-                        robot.move_pose(*[0.326,-0.015,0.364,-2.175,1.476,-2.178])
-                        robot.move_pose(*[0 ,-0.284, 0.325, 2.928, 1.346, 1.383])
+                    robot.place_from_pose(*[0.326,-0.015,0.314,-2.232,1.471,-2.234])
+                    robot.move_pose(*[0.326,-0.015,0.364,-2.175,1.476,-2.178])
+                    robot.move_pose(*[0 ,-0.284, 0.325, 2.928, 1.346, 1.383])
 
-                except NiryoRobotException as e:
-                    sys.stderr.write(str(e))
+            except NiryoRobotException as e:
+                sys.stderr.write(str(e))
                 
-                robot.close_connection()
-                self.pub_niryo.publish(2)
+            robot.close_connection()
+            self.pub_niryo.publish(2)
 
                 #rospy.signal_shutdown("Task completed") #Shutdown the ROS after c
 if __name__ == "__main__":
